990f2399627e26bd7541f9954dba49543cba4473
stefanniculae2208 <stefan.niculae2208@gmail.com>
2022-08-29 13:29:28 +0300

    my commit

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ git commit -m "my commit"
 git push https://github.com/stefanniculae2208/DELILA_TQDC.git main
 
 expect "Username for 'https://github.com': "
-send -- "stefanniculae2208"
+send -- "stefanniculae2208\n"
 
 expect "Password for 'https://stefanniculae2208@github.com': "
-send -- "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"
+send -- "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\n"
